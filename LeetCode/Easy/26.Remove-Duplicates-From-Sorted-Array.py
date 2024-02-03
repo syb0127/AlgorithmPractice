df888b37a76f645744120b9d79a5507546769058
Diana Seo <syb0127@berkeley.edu>
2024-02-03 23:04:17 +0900

    Prob: Added explanation for #26 solution

---
+++ LeetCode/Easy/26.Remove-Duplicates-From-Sorted-Array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        numslength = 0
+        seen = []
+        copy = []
+        for j in range(0, len(nums)):
+            copy.append(nums[j])
+        for i in range(0, len(copy)):
+            if copy[i] not in seen:
+                numslength += 1
+                seen.append(copy[i])
+            else:
+                nums.remove(copy[i])
+        return numslength;
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
