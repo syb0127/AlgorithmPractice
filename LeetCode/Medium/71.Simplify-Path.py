5a30fc7266ee8f4b9024bd0822ef773a6bb49537
Diana Seo <syb0127@berkeley.edu>
2024-02-18 21:59:44 +0900

    Prob: Added explanation for #71 solution

---
+++ LeetCode/Medium/71.Simplify-Path.py
@@ -0,0 +1,14 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+
+        split_path = path.split("/")
+        stack = []
+
+        for path in split_path:
+          if len(path)>0 and path != '.':
+                if path == '..' and stack:
+                    stack.pop()
+                elif path != '..':
+                    stack.append(path)
+        
+        return "/"+"/".join(stack)
